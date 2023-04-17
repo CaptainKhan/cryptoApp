5c402a0cef3eb7d63069e6c08f76b654774b55ba
Aga KHAN <93132150+CaptainKhan@users.noreply.github.com>
2023-04-17 16:21:46 +0400

    Update cryptoApi.js

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,8 +1,8 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const cryptoApiHeaders = {
-    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
-    'X-RapidAPI-Key': 'a31e4e97edmsh67f355377e94fb8p14b2b4jsnc8c4b7ed1688',
+// there was api
+
 }
 
 const baseUrl = 'https://coinranking1.p.rapidapi.com/';
